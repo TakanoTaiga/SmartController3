4f13078d93b36c87feeb1a5e069016d3376f0b7b
Taiga Takano <taiga.takano@tier4.jp>
2023-03-20 23:30:45 +0900

    delete old code

--- SmartController3/Interface/ROSInfomation.swift
+++ SmartController3/Interface/ROSInfomation.swift
@@ -18,64 +18,6 @@ struct ROSInfomation: View {
             RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(.brown)
                 .opacity(0.1)
-            
-            
-//            VStack{
-//                Spacer()
-//
-//                HStack {
-//                    VStack(alignment: .leading) {
-//                        if(ROSConnectHandler.nodeConnectionParameter.hostName != ""){
-//                            Text("[Device]: \(ROSConnectHandler.nodeConnectionParameter.hostName)")
-//                                .font(.title)
-//                        }
-//
-//                        if(ROSConnectHandler.nodeConnectionParameter.nodeName != ""){
-//                            Text("[Node]: \(ROSConnectHandler.nodeConnectionParameter.nodeName)")
-//                                .padding(.bottom)
-//                                .font(.title)
-//                        }
-//
-//
-//                        if(ROSConnectHandler.nodeConnectionParameter.state != ServiceState.ready){
-//                            Text("[L4SC3]: Service not connect")
-//                                .foregroundColor(.red)
-//                        }
-//                    }
-//                    Spacer()
-//                }
-//
-//                Spacer()
-//
-//                if ROSConnectHandler.nodeConnectionParameter.state != ServiceState.ready {
-//                    Image(systemName: "xmark.circle")
-//                        .foregroundColor(.red)
-//                        .font(.title)
-//                        .onAppear(){
-////                            gccTimer?.invalidate()
-////                            gccUpdateTimer?.invalidate()
-//                        }
-//                        .onDisappear(){
-////                            GCC.NWSetup(host: ROSConnectHandler.nodeConnectionParameter.nodeIP)
-////                            gccTimer?.invalidate()
-////                            gccTimer = Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true){ _ in
-////                                GCC.sendGameControllerStatus(force: false)
-////                            }
-////
-////                            gccUpdateTimer?.invalidate()
-////                            gccUpdateTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true){ _ in
-////                                if(GCC.needUpdate){
-////                                    GCC.sendGameControllerStatus(force: true)
-////                                }
-////                                GCC.needUpdate = true
-////                           }
-//                        }
-//                }
-//                Spacer()
-            //}
-        }
-        .onTapGesture {
-            //ROSConnectHandler.resetStatus()
         }
     }
 }
