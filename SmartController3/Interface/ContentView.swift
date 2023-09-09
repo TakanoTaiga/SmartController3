851e2a336220682032f17205cf525e0e4568db7a
Taiga Takano <ttttghghnb554z@outlook.jp>
2023-09-09 11:26:01 +0900

    Add MRM

--- SmartController3/Interface/ContentView.swift
+++ SmartController3/Interface/ContentView.swift
@@ -4,156 +4,195 @@
 //
 //  Created by Taiga Takano on 2022/06/18.
 //
-
 import SwiftUI
-
 
 struct ContentView: View {
     @ObservedObject var nodeConnectionClassObject = NodeConnection()
-    
-    @State private var show_settings = false
-    @State private var show_systeminfo = true
-    @State private var show_slowmode = false
-    @State private var show_big_slowmode = false
-    
-    @State private var press_gear = false
-    
-    @State private var need_longpress = false
+
+    @State private var showSettings = false
+    @State private var showSystemInfo = true
+    @State private var showSlowMode = false
+    @State private var showBigSlowMode = false
+    @State private var showEnergency = true
+    @State private var showVersion = false
+    @State private var pressGear = false
+    @State private var needLongPress = false
+
+    private let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    private let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue.opacity(0.2), .green.opacity(0.3)]), startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
-                .onTapGesture {
-                    show_settings = false
-                }
-            
-            VStack {
-                ZStack {
-                    NodeStatus(nodeConnectionClassObject: nodeConnectionClassObject)
-                        .padding(.horizontal)
-                        .blur(radius: need_longpress ? 5 : 0.0)
-                        .opacity(need_longpress ? 0.7 : 1.0)
-                        .animation(.easeIn(duration: 0.1), value: need_longpress)
-                    HStack{
-                        Spacer()
-                        
-                        Text("長押ししてください")
-                            .font(.callout)
-                            .foregroundStyle(.secondary)
-                            .padding(.horizontal)
-                            .opacity(need_longpress ? 1.0 : 0.0)
-                            .animation(.easeOut(duration: 0.2), value: need_longpress)
-                        
-                        Image(systemName: "gear")
-                            .font(.title3)
-                            .foregroundStyle(.secondary)
-                            .padding(.horizontal)
-                            .opacity(press_gear ? 0.5 : 1.0)
-                    }
-                }
-                
-                ScrollView(showsIndicators: false){
-                    VStack{
-                        if show_big_slowmode {
-                            SlowMode(nodeConnectionClass: nodeConnectionClassObject)
-                                .frame(height: 200)
-                                .padding(.bottom)
-                                .onAppear(){
-                                    show_slowmode = false
-                                }
-                        }
-                        
-                        ControllerInfomationWidget(nodeConnectionClass: nodeConnectionClassObject)
-                            .padding(.bottom)
-                        
-                        if show_systeminfo {
-                            SystemInfo(NodeConnectionClassObject: nodeConnectionClassObject)
-                                .frame(height: 200)
-                                .padding(.bottom)
-                        }
-                        
-                        if show_slowmode {
-                            SlowMode(nodeConnectionClass: nodeConnectionClassObject)
-                                .frame(height: 50)
-                                .padding(.bottom)
-                                .onAppear(){
-                                    show_big_slowmode = false
-                                }
-                        }
-                        
-                    }
-                }
-                .padding([.leading, .bottom, .trailing])
-            }
-            .ignoresSafeArea(edges: [.bottom])
-            .onTapGesture {
-                show_settings = false
-            }
-            
-            VStack{
-                Rectangle()
-                    .frame(height: 90)
-                    .ignoresSafeArea()
-                    .opacity(0.0001)
-                    .gesture(
-                        LongPressGesture(minimumDuration: 0.3)
-                        .onEnded({ _ in
-                            show_settings.toggle()
-                            NSLog("press")
-                        })
-                        .onChanged({ _ in
-                            press_gear = true
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                                press_gear = false
-                                if !show_settings {
-                                    need_longpress = true
-                                }
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                    need_longpress = false
-                                }
-                            }
-                            
-                            if show_settings{
-                                show_settings = false
-                            }
-                        })
-                    )
-                Spacer()
-                
-            }
-            
-            VStack{
-                Spacer()
-                ZStack {
-                    Rectangle()
-                        .foregroundStyle(.quaternary)
-                        .background(.ultraThinMaterial)
-                        .shadow(radius: 10)
-                    
-                    VStack{
-                        HStack {
-                            Text("設定")
-                                .font(.headline)
-                            Spacer()
-                        }
-                        
-                        Toggle("デバッグパネル", isOn: $show_systeminfo)
-                        
-                        Toggle("スローモード", isOn: $show_slowmode)
-                        
-                        Toggle("最大 スローモード", isOn: $show_big_slowmode)
-                            
+            baseView()
+            settingsView()
+            longPressIndicator()
+        }
+        .onAppear(){
+            showSystemInfo = UserDefaults.standard.object(forKey: "showSystemInfo") as? Bool ?? true
+            showSlowMode = UserDefaults.standard.object(forKey: "showSlowMode") as? Bool ?? false
+            showBigSlowMode = UserDefaults.standard.object(forKey: "showBigSlowMode") as? Bool ?? false
+            showEnergency = UserDefaults.standard.object(forKey: "showEnergency") as? Bool ?? true
 
-                        Spacer()
-                    }.padding(.all)
-                    
-                }
-                .frame(height: 300)
-                .ignoresSafeArea()
-                .offset(y: show_settings ? 100 : 600)
-                .animation(.easeInOut(duration: 0.2), value: show_settings)
+        }
+        .onDisappear(){
+            UserDefaults.standard.set(showSystemInfo, forKey: "showSystemInfo")
+            UserDefaults.standard.set(showSlowMode, forKey: "showSlowMode")
+            UserDefaults.standard.set(showBigSlowMode, forKey: "showBigSlowMode")
+            UserDefaults.standard.set(showEnergency, forKey: "showEnergency")
+            
+        }
+    }
+
+    // MARK: - Private Helpers
+    private func baseView() -> some View {
+        ZStack {
+            backgroundGradient()
+            VStack {
+                nodeStatusView()
+                scrollViewContent()
             }
+            .onTapGesture {
+                showSettings = false
+            }
+        }
+    }
+
+    private func backgroundGradient() -> some View {
+        LinearGradient(gradient: Gradient(colors: [.blue.opacity(0.2), .green.opacity(0.3)]), startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+            .onTapGesture {
+                showSettings = false
+            }
+    }
+
+    private func nodeStatusView() -> some View {
+        ZStack {
+            NodeStatus(nodeConnectionClassObject: nodeConnectionClassObject)
+                .padding(.horizontal)
+                .blur(radius: needLongPress ? 5 : 0.0)
+                .opacity(needLongPress ? 0.7 : 1.0)
+                .animation(.easeIn(duration: 0.1), value: needLongPress)
+            HStack {
+                Spacer()
+                longPressHint()
+                gearIcon()
+            }
+        }
+    }
+
+    private func scrollViewContent() -> some View {
+        ScrollView(showsIndicators: false) {
+            VStack {
+                if showBigSlowMode {
+                    SlowMode(nodeConnectionClass: nodeConnectionClassObject)
+                        .frame(height: 200)
+                        .padding(.bottom)
+                        .onAppear { showSlowMode = false }
+                }
+                ControllerInfomationWidget(nodeConnectionClass: nodeConnectionClassObject).padding(.bottom)
+                if showSystemInfo {
+                    SystemInfo(NodeConnectionClassObject: nodeConnectionClassObject)
+                        .frame(height: 200)
+                        .padding(.bottom)
+                }
+                if showSlowMode {
+                    SlowMode(nodeConnectionClass: nodeConnectionClassObject)
+                        .frame(height: 50)
+                        .padding(.bottom)
+                        .onAppear { showBigSlowMode = false }
+                }
+                
+                if showEnergency {
+                    EmergencyCall()
+                        .frame(height: 150)
+                }
+            }
+            .padding([.leading, .bottom, .trailing])
+        }
+    }
+
+    private func longPressHint() -> some View {
+        Text("長押ししてください")
+            .font(.callout)
+            .foregroundStyle(.secondary)
+            .opacity(needLongPress ? 1.0 : 0.0)
+            .animation(.easeOut(duration: 0.2), value: needLongPress)
+    }
+
+    private func gearIcon() -> some View {
+        Image(systemName: "gear")
+            .font(.title3)
+            .foregroundStyle(.secondary)
+            .padding(.horizontal)
+            .opacity(pressGear ? 0.5 : 1.0)
+    }
+
+    private func settingsView() -> some View {
+        VStack {
+            Spacer()
+            ZStack {
+                Rectangle()
+                    .foregroundStyle(.quaternary)
+                    .background(.ultraThinMaterial)
+                    .shadow(radius: 10)
+                VStack {
+                    HStack {
+                        Text(showVersion ? "\(version)(\(build))" : "設定")
+                            .font(.headline)
+                            .onTapGesture {
+                                showVersion.toggle()
+                            }
+                            .animation(.easeInOut(duration: 0.2), value: showVersion)
+                        Spacer()
+                    }
+                    Toggle("デバッグパネル", isOn: $showSystemInfo)
+                    Toggle("スローモード", isOn: $showSlowMode)
+                    Toggle("最大 スローモード", isOn: $showBigSlowMode)
+                    Toggle("緊急停止", isOn: $showEnergency)
+                    Spacer()
+                }.padding(.all)
+            }
+            .frame(height: 300)
+            .offset(y: showSettings ? 100 : 600)
+            .animation(.easeInOut(duration: 0.2), value: showSettings)
+        }
+    }
+
+    private func longPressIndicator() -> some View {
+        VStack {
+            Rectangle()
+                .frame(height: 90)
+                .opacity(0.0001)
+                .gesture(longPressGesture())
+            Spacer()
+        }
+    }
+
+    private func longPressGesture() -> some Gesture {
+        LongPressGesture(minimumDuration: 0.3)
+            .onEnded { _ in
+                showSettings.toggle()
+                print("press")
+            }
+            .onChanged { _ in
+                handleLongPressChange()
+            }
+    }
+
+    private func handleLongPressChange() {
+        pressGear = true //for gear animation
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.31) {
+            pressGear = false //for gear animation
+            if !showSettings{
+                needLongPress = true
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                needLongPress = false
+            }
+        }
+
+        if showSettings {
+            showSettings = false
         }
     }
 }
--- SmartController3/Interface/EmergencyCall.swift
+++ SmartController3/Interface/EmergencyCall.swift
@@ -9,37 +9,72 @@ import SwiftUI
 import Network
 import SystemConfiguration
 import CoreTelephony
+import TriggerSlider
 
 struct EmergencyCall: View {
-    @State var nwcCallback : NWConnection?
-    @State var queue = DispatchQueue(label: "EmergencyCall", qos: .background , attributes: .concurrent)
+    @State private var nwcCallback : NWConnection?
+    @State private var queue = DispatchQueue(label: "EmergencyCall", qos: .background , attributes: .concurrent)
+    @State private var offset: CGFloat = 0.0
+    
+    @State private var emergencyCallStatus = false
     
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(.yellow)
             
-            VStack{
-                Text("EMERGENCY")
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                Button(action: {
-                    nwcCallback = NWConnection(host: "255.255.255.255", port: 64202, using: .udp)
-                    nwcCallback?.start(queue: self.queue)
-                    send(item: "EMERGENCY")
-                    
-                }, label: {
-                    Circle()
-                        .frame(width: 90)
-                        .foregroundColor(.red)
-                        .shadow(radius: 5)
-                })
+            VStack {
+                Spacer()
                 
-                Text("STOP")
+                Text("EMERGENCY STOP")
                     .fontWeight(.bold)
-                    .foregroundColor(.black)
+                    .font(.title)
+                    .opacity(emergencyCallStatus ? 0.5 : 1.0)
+                    .animation(.easeIn(duration: 0.4), value: emergencyCallStatus)
+                
+                ZStack {
+                    VStack {
+                        Text("緊急停止措置完了")
+                            .fontWeight(.bold)
+                            .font(.title)
+                            .offset(y: -20)
+                            .opacity(emergencyCallStatus ? 1.0 : 0.0)
+                            .animation(.easeIn(duration: 0.4), value: emergencyCallStatus)
+                        Text("解除するには長押し")
+                            .offset(y: -20)
+                            .opacity(emergencyCallStatus ? 1.0 : 0.0)
+                            .animation(.easeIn(duration: 0.4), value: emergencyCallStatus)
+                    }
+                    
+                    TriggerSlider(sliderView: {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous).fill(Color.red)
+                            .overlay(Image(systemName: "arrow.right").font(.system(size: 30)))
+                    }, textView: {
+                        Text("スライドして緊急停止").foregroundColor(Color.black)
+                    },
+                                  backgroundView: {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .fill(Color.orange.opacity(0.5))
+                    }, offsetX: $offset,
+                                  didSlideToEnd: {
+                        print("Triggered right direction slider!")
+                        nwcCallback = NWConnection(host: "255.255.255.255", port: 64202, using: .udp)
+                        nwcCallback?.start(queue: self.queue)
+                        send(item: "EMERGENCY")
+                        emergencyCallStatus = true
+                    }, settings: TriggerSliderSettings(sliderViewVPadding: 5, slideDirection: .right))
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 20)
+                    .opacity(emergencyCallStatus ? 0.0 : 1.0)
+                    .animation(.easeIn(duration: 0.4), value: emergencyCallStatus)
+                }
             }
         }
+        .gesture(LongPressGesture(minimumDuration: 0.3)
+            .onEnded { _ in
+                emergencyCallStatus = false
+                offset = 0
+            })
     }
     
     func send(item : String){
